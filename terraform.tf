691d8d5cbf834f2941ec31d2cdb0acb81f5fec76
Kerim Satirli <k.satirli@gmail.com>
2023-04-05 14:39:08 +0200

    bumps AWS, HCP, and Vault providers

--- terraform.tf
+++ terraform.tf
@@ -12,25 +12,25 @@ terraform {
 
   # see https://developer.hashicorp.com/terraform/language/settings#specifying-provider-requirements
   required_providers {
-    # see https://registry.terraform.io/providers/hashicorp/boundary/1.1.3
+    # see https://registry.terraform.io/providers/hashicorp/boundary/1.1.4
     boundary = {
       source  = "hashicorp/boundary"
-      version = "1.1.3"
+      version = "1.1.4"
     }
 
-    # see https://registry.terraform.io/providers/hashicorp/hcp/0.53.0/
+    # see https://registry.terraform.io/providers/hashicorp/hcp/0.56.0/
     hcp = {
       source  = "hashicorp/hcp"
-      version = ">= 0.53.0, < 1.0.0"
+      version = ">= 0.56.0, < 1.0.0"
     }
 
-    # see https://registry.terraform.io/providers/hashicorp/vault/3.12.0
+    # see https://registry.terraform.io/providers/hashicorp/vault/3.14.0
     vault = {
       source  = "hashicorp/vault"
-      version = "3.12.0"
+      version = "3.14.0"
     }
   }
 
   # see https://developer.hashicorp.com/terraform/language/settings#specifying-a-required-terraform-version
-  required_version = ">= 1.3.0"
+  required_version = ">= 1.4.0"
 }
