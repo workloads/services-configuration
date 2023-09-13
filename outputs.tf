2a205ec7882e8626d438dfe165c508a758cc5d0f
Kerim Satirli <k.satirli@gmail.com>
2023-09-13 15:02:44 +0200

    work up for Boundary configuration

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,13 @@
+output "boundary_scope_global" {
+  description = "Exported Attributes for `boundary_scope.global`."
+  value       = boundary_scope.global
+}
+
+output "boundary_scope_organization" {
+  description = "Exported Attributes for `boundary_scope.organization`."
+  value       = boundary_scope.organization
+}
+
 output "datadog_urls" {
   description = "URLs for Datadog Services."
 
@@ -5,6 +15,12 @@ output "datadog_urls" {
     hcp_streaming_logs = "https://app.datadoghq.com/logs?query=service%3A%22HCP%20-%20HCP-Streaming%22"
   }
 }
+
+output "hcp_boundary_cluster" {
+  description = "Exported Attributes for `hcp_boundary_cluster` data source."
+  value       = data.hcp_boundary_cluster.main
+}
+
 output "hcp_vault_cluster" {
   description = "Exported Attributes for `hcp_vault_cluster` data source."
   value       = data.hcp_vault_cluster.main
@@ -19,26 +35,3 @@ output "vault_kv_v2_vault_kv_secret_backend_v2" {
   description = "Exported Attributes for `module.vault_kv_v2.vault_kv_secret_backend_v2`."
   value       = module.vault_kv_v2.vault_kv_secret_backend_v2
 }
-
-output "hcp_boundary_cluster" {
-  description = "Exported Attributes for `hcp_boundary_cluster` data source."
-  value       = data.hcp_boundary_cluster.main
-}
-
-output "boundary_scope_global" {
-  description = "Exported Attributes for `boundary_scope.global`."
-  value       = boundary_scope.global
-}
-
-output "boundary_scope_organization" {
-  description = "Exported Attributes for `boundary_scope.organization`."
-  value       = boundary_scope.organization
-}
-
-#output "boundary_scope_csp_projects" {
-#  description = "Exported Attributes for `boundary_scope.csp_projects`."
-#
-#  value = {
-#    for csp in boundary_scope.csp_projects : csp => csp.name
-#  }
-#}
